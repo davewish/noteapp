32249bc299439418f48b972a4c78649b3f35757b
dawit tekle <dvwish9214@gmail.com>
2023-08-15 23:30:55 +0300

    done

--- src/components/note.jsx
+++ src/components/note.jsx
@@ -11,57 +11,46 @@ const Note = () => {
   const addNote = () => {
     setShhouldFormVisible(true);
   };
-  const saveNote=(note)=> {
-    
-    setNoteList((preNote)=> [...preNote, note])
-    setShhouldFormVisible(false)
-    saveData(note)
-    
-  }
-   const saveData=  (note)=> {
-     
-  
-       const notes=noteList;
-       if(note) {
-        notes.push(note)
-       }
-      
-       const noteString=JSON.stringify(notes)
-      localStorage.setItem("note", noteString)
-   }
-   const deleteData=(id)=> {
-    const notes=noteList.filter(note=>note.id !== id)
-    const noteString=JSON.stringify(notes)
-      localStorage.setItem("note", noteString)
+  const saveNote = (note) => {
+    setNoteList((preNote) => [...preNote, note]);
+    setShhouldFormVisible(false);
+    saveData(note);
+  };
+  const saveData = (note) => {
+    const notes = noteList;
+    if (note) {
+      notes.push(note);
+    }
 
-   }
-   const getData=()=> {
-
-    const data=localStorage.getItem("note")
-     const dataObj=JSON.parse(data)
-     setNoteList(dataObj)
-   }
-   useEffect(()=> {
-     if(noteList.length===0) {
-        getData()
-     }
-
-   
-
-   },[])
-   const deleteNote=(id)=> {
-      const notes= noteList.filter((note=>note.id !== id))
-      setNoteList(notes)
-       deleteData(id)
-
-   }
-   
+    const noteString = JSON.stringify(notes);
+    localStorage.setItem("note", noteString);
+  };
+  const deleteData = (id) => {
+    const notes = noteList.filter((note) => note.id !== id);
+    const noteString = JSON.stringify(notes);
+    localStorage.setItem("note", noteString);
+  };
+  const getData = () => {
+    const data = localStorage.getItem("note");
+    const dataObj = JSON.parse(data);
+    setNoteList(dataObj);
+  };
+  useEffect(() => {
+    if (noteList.length === 0) {
+      getData();
+    }
+  }, []);
+  const deleteNote = (id) => {
+    const notes = noteList.filter((note) => note.id !== id);
+    setNoteList(notes);
+    deleteData(id);
+  };
 
   return (
     <>
       <div className="note-container">
         {noteList.map((note) => (
-          <NoteItem key={note.id} note={note}  deleteNote={deleteNote}/>
+          <NoteItem key={note.id} note={note} deleteNote={deleteNote} />
         ))}
 
         <div className="note add-note" onClick={addNote}>
